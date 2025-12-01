b666adc2b1ca2aae0a32a86d25cd2d8595d341ab
jaxwong <wongzhengwei2003@gmail.com>
2025-12-01 16:14:23 +0800

    Add redux store

--- client/app/(components)/Sidebar/index.tsx
+++ client/app/(components)/Sidebar/index.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react'
 import Image from 'next/image';
+import { LockIcon } from 'lucide-react';
 
 const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
@@ -22,7 +23,15 @@ const Sidebar = () => {
         {/*TEAM */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
           <Image src="/logo.png" alt="Logo" width={40} height={40}/>
+          <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+            EDROH TEAM
+          </h3>
+          <div className="mt-1 flex items-start gap-2">
+            <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+            <p className="text-xs text-gray-500">Private</p>
+          </div>
         </div>
+        {/* NAVBAR LINKS */}
       </div>
     </div>
   )
