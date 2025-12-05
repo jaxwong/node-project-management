cacb2e1801e4395d8ddb62c3a29a9fbb3d122905
Lenovo <Lenovo@MacBook-Pro.local>
2025-12-05 16:58:39 +0800

    Add homepage charts

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HomePage from "./home/page";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        hi
-      </main>
+      <HomePage />
   );
 }
